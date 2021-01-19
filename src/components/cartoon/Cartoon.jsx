7e665f82cf55d9213d3a907f8c62a4c0b1d36363
leee1846 <leee1846@gmail.com>
2021-01-19 17:23:53 +0900

    fix: cartoon페이지 컴포넌트 분리

--- src/components/cartoon/Cartoon.jsx
+++ src/components/cartoon/Cartoon.jsx
@@ -1,8 +1,9 @@
-import React, { Fragment, useRef, useState } from "react";
+import React, { useState } from "react";
 import * as Styled from "./Cartoon.style";
 import image1 from "../../images/pic1.jpg";
 import image2 from "../../images/pic2.jpg";
 import image3 from "../../images/pic3.jpg";
+import CartoonItem from "../cartoonItem/CartoonItem";
 
 const imageData = [
   {
@@ -29,46 +30,20 @@ const imageData = [
 ];
 
 function Cartoon() {
-  const contentElem = useRef([]);
-  const imageElem = useRef([]);
   const [dataList, setDataList] = useState(imageData);
-
-  const dataMap = dataList.map((data) => {
-    return (
-      <Fragment key={data.id}>
-        <Styled.PicContainer>
-          <Styled.Image
-            src={data.pic}
-            display={data.display}
-            ref={(ref) => imageElem.current.push(ref)}
-          ></Styled.Image>
-        </Styled.PicContainer>
-        <Styled.ContentContainer>
-          <Styled.Content ref={(ref) => contentElem.current.push(ref)}>
-            <p>{data.content}</p>
-          </Styled.Content>
-        </Styled.ContentContainer>
-      </Fragment>
-    );
-  });
-
-  window.addEventListener("scroll", () => {
-    contentElem.current.forEach((content, idx) => {
-      const boundingRect = content.getBoundingClientRect();
-
-      if (
-        boundingRect.top < window.innerHeight * 0.5 &&
-        boundingRect.top > window.innerHeight * 0.4
-      ) {
-        // setDataList((dataList) => (dataList[idx].display = ""));
-        // setDataList(dataList);
-      }
-    });
-  });
 
   return (
     <>
-      <Styled.Section>{dataMap}</Styled.Section>
+      <Styled.Section>
+        {dataList.map((data) => (
+          <CartoonItem
+            key={data.id}
+            pic={data.pic}
+            display={data.display}
+            content={data.content}
+          />
+        ))}
+      </Styled.Section>
     </>
   );
 }
